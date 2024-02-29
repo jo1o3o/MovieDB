f573be398153cea2fac04db4d8d4a6405a2b12f7
Jin Oh <jinsoh720@gmail.com>
2024-02-29 12:29:54 -0800

    Initialized spring boot application.

---
+++ server/src/main/java/com/jin/oh/moviedb/MovieDatabaseApplication.java
@@ -0,0 +1,14 @@
+package com.jin.oh.moviedb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MovieDatabaseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MovieDatabaseApplication.class, args);
+		System.out.print("Movie Database application started");
+	}
+
+}
